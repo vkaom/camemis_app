e9f76e8562f988e2b4a5e899d5eb9e044d32aa14
Rada <khlouk.rada@gmail.com>
2016-07-20 15:11:51 +0700

    - update chat list with refresh control and mocking data from remote source

--- src/components/chatList.js
+++ src/components/chatList.js
@@ -11,32 +11,91 @@ import {
   View,
   Image,
   ScrollView,
-  PixelRatio
+  PixelRatio,
+  RefreshControl
 } from 'react-native';
 
 import CamemisToolbar from './toolbar';
 
+var REQUEST_URL = 'https://raw.githubusercontent.com/facebook/react-native/master/docs/MoviesExample.json';
+var personList = [
+  // {name: 'Ethan Fox', img: require('../images/person-1.jpg'), lastChatText: 'Sweet! I heard they had a great time over at the cabin. Next time we should bring the croquest set.'},
+  // {name: 'John Son', img: require('../images/person-2.jpg'), lastChatText: 'You: Nope! We are good!'},
+  // {name: 'Marry', img: require('../images/person-3.jpg'), lastChatText: 'Oh yeah? Because I think no.:)'},
+  // {name: 'Steven', img: require('../images/person-4.png'), lastChatText: 'Oh yeah? Because I think no.:)'},
+  // {name: 'Hungary', img: require('../images/person-5.jpg'), lastChatText: 'Oh yeah? Because I think no.:)'},
+];
 module.exports = class ChatList extends Component {
-  render() {
-    var personList = [
-      {name: 'Ethan Fox', img: require('../images/person-1.jpg'), lastChatText: 'Sweet! I heard they had a great time over at the cabin. Next time we should bring the croquest set.'},
-      {name: 'John Son', img: require('../images/person-2.jpg'), lastChatText: 'You: Nope! We are good!'},
-      {name: 'Marry', img: require('../images/person-3.jpg'), lastChatText: 'Oh yeah? Because I think no.:)'},
-      {name: 'Steven', img: require('../images/person-4.png'), lastChatText: 'Oh yeah? Because I think no.:)'},
-      {name: 'Hungary', img: require('../images/person-5.jpg'), lastChatText: 'Oh yeah? Because I think no.:)'},
-    ];
+  constructor(props) {
+    super(props);
+    this.state = {
+      refreshing: false,
+      loaded: false,
+    };
+    //this._renderDrawerContent = this._renderDrawerContent.bind(this);
+  }
+  componentDidMount() {
+    this._fetchData();
+  }
+  _onRefresh() {
+    this._fetchData();
+  }
+  _fetchData() {
+    this.setState({refreshing: true});
+    fetch(REQUEST_URL)
+      .then((response) => response.json())
+      .then((responseData) => {
+        //console.log(responseData.movies);
+        responseData.movies.map(function(object, i){
+          personList.unshift({name: object.title, img: object.posters.thumbnail, lastChatText: 'Oh yeah?'});
+        });
+        this.setState({
+          refreshing: false,
+          loaded: true,
+        });
+      })
+      .done();
+  }
+  _renderLoadingView() {
     return (
       <View style={{flex: 1}}>
         <View>
           <CamemisToolbar navigator={this.props.navigator} title="Chat" />
         </View>
-        <ScrollView style={styles.container}>
+        <View style={{flex: 1, flexDirection: 'row', justifyContent: 'center', alignItems: 'center',backgroundColor: '#FFFFFF',}}>
+          <Text>
+            Loading...
+          </Text>
+        </View>
+      </View>
+    );
+  }
+  render() {
+    if (!this.state.loaded) {
+      return this._renderLoadingView();
+    }
+    return (
+      <View style={{flex: 1}}>
+        <View>
+          <CamemisToolbar navigator={this.props.navigator} title="Chat" />
+        </View>
+        <ScrollView
+          style={styles.container}
+          contentContainerStyle={{paddingTop:10}}
+          refreshControl={
+            <RefreshControl
+              refreshing={this.state.refreshing}
+              onRefresh={this._onRefresh.bind(this)}
+            />
+          }
+        >
           {
             personList.map(function(object, i){
               return (
                 <View style={styles.chatItemList} key={i}>
                   <Image
-                    source={object.img}
+                    //source={object.img}
+                    source={{uri: object.img}}
                     style={styles.thumbnail}
                   />
                   <View style={styles.rightContainer}>
@@ -58,7 +117,6 @@ const styles = StyleSheet.create({
     flex: 1,
     flexDirection: 'column',
     backgroundColor: '#FFFFFF',
-    paddingTop: 10,
   },
   chatItemList: {
     flexDirection: 'row',
