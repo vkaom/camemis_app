eff8f4815e3cc5d7d78d8d7a6068b713247517f5
veasna <veasnasor@gmail.com>
2016-08-06 14:46:29 +0700

    change reducer setting

--- src/reducers/setting.js
+++ src/reducers/setting.js
@@ -3,9 +3,9 @@ import * as types from '../actions/actionTypes';
 export default function setting(state={SCHOOL_NAME:null, MOBILE_URL: null , MOBILE_CODE: null, LANGUAGE: null}, action = {}) {
   switch (action.type) {
     case types.DO_SETTING:
-      var a = state;
+      var a = Object.assign({},state);
       a[action.key] = action.value;
-      return Object.assign({}, state, a);
+      return a;
     default:
       return state;
   }
