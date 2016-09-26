832e777fd4e2a506b5ed420cfdaaf8611f5ebff6
Bigmac <bigmac@Bigmacs-MacBook-Pro.local>
2016-09-26 17:47:00 +0700

    add new files

---
+++ android/app/src/main/java/com/camemis_app/MainApplication.java
@@ -0,0 +1,42 @@
+package com.camemis_app;
+
+import android.app.Application;
+import android.util.Log;
+
+import com.facebook.react.ReactApplication;
+import com.facebook.react.ReactInstanceManager;
+import com.facebook.react.ReactNativeHost;
+import com.facebook.react.ReactPackage;
+import com.facebook.react.shell.MainReactPackage;
+
+import java.util.Arrays;
+import java.util.List;
+
+
+import com.babisoft.ReactNativeLocalization.ReactNativeLocalizationPackage;
+import com.dieam.reactnativepushnotification.ReactNativePushNotificationPackage;
+
+
+public class MainApplication extends Application implements ReactApplication {
+
+  private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
+    @Override
+    protected boolean getUseDeveloperSupport() {
+      return BuildConfig.DEBUG;
+    }
+
+    @Override
+    protected List<ReactPackage> getPackages() {
+      return Arrays.<ReactPackage>asList(
+          new MainReactPackage(),
+          new ReactNativeLocalizationPackage(),
+          new ReactNativePushNotificationPackage()
+      );
+    }
+  };
+
+  @Override
+  public ReactNativeHost getReactNativeHost() {
+      return mReactNativeHost;
+  }
+}
